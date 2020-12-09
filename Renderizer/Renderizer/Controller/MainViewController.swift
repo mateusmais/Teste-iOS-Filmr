d95094088b083af3af53997f9cc6744e3a0bed66
Mateus Ferreira <mateusmais@icloud.com>
2020-12-09 02:18:18 -0300

    Function Save Image in Camera Roll ok !

--- Renderizer/Renderizer/Controller/MainViewController.swift
+++ Renderizer/Renderizer/Controller/MainViewController.swift
@@ -16,9 +16,11 @@ final class MainViewController: UIViewController {
     //MARK: - Variables
     //Variables
     private let baseView = MainView()
+    private var imageToSave = UIImageView()
     private var network = Network()
-     
-
+    
+    
+    
     //MARK: - Load View
     //Load View
     
@@ -34,10 +36,8 @@ final class MainViewController: UIViewController {
     /// - Parameter animated: True Or False
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         navigationController?.setNavigationBarHidden(false, animated: animated)
         navigationController?.setToolbarHidden(false, animated: animated)
-        
     }
     
     //MARK: - View Did Load
@@ -50,20 +50,58 @@ final class MainViewController: UIViewController {
         
         //Call Request
         network.fetchImage()
+        
+        //Setup Toolbar
+        setupToolbar()
     }
 }
 
-
+//MARK: - Network Delegate
 extension MainViewController: NetworkDelegate {
     func didUpdateImage(_ network: Network, _ networkData: NetworkModel) {
         DispatchQueue.main.async {
             self.baseView.randomImage.kf.setImage(with: networkData.small)
-            print("Entrou aqui")
+            self.imageToSave.kf.setImage(with: networkData.small)
         }
     }
     
     func didFailWithError(_ error: Error) {
         print(error)
     }
+}
+
+//MARK: - Toolbar
+extension MainViewController {
     
+    /// Setup Toolbar With Update And Actions
+    private func setupToolbar() {
+        
+        updateToolbarItems()
+        
+        baseView.filtersBarButton.addTarget(self, action: #selector(didFiltersButtonClicked), for: .touchUpInside)
+        
+        baseView.saveBarButton.addTarget(self, action: #selector(didSaveImageButtonClicked), for: .touchUpInside)
+        
+    }
+    
+    /// Update Toolbar
+    private func updateToolbarItems() {
+        toolbarItems = baseView.getBarButtonItems()
+    }
+}
+
+//MARK: - Actions
+extension MainViewController {
+    
+    @objc private func didFiltersButtonClicked() {
+        //Function
+    }
+    
+    @objc private func didSaveImageButtonClicked() {
+        guard let image = self.imageToSave.image else {
+            print(Error.self)
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    }
 }
