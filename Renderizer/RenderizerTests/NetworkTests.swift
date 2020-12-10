9b17d0e8c4112ace08934c50bbfcec9ff8b5a3a2
Mateus Ferreira <mateusmais@icloud.com>
2020-12-09 21:20:25 -0300

    Test Network

--- Renderizer/RenderizerTests/NetworkTests.swift
+++ Renderizer/RenderizerTests/NetworkTests.swift
@@ -13,13 +13,26 @@ class NetworkTests: XCTestCase {
 
     var network: Network?
     
+    
+    /// Set Up
     override func setUp() {
         super.setUp()
         network = Network()
     }
     
+    
+    /// Tear Down
     override func tearDown() {
         super.tearDown()
         network = nil
+    }
+    
+    
+    /// Test FetchImage
+    func testFetchImage(){
+        let url = "https://api.unsplash.com/photos/random?client_id=\(Key.accessKey)"
+        let test: ()? = network?.performRequest(with: url)
+        XCTAssertNotNil(test, "")
+        XCTAssertTrue((test != nil))
     }
 }
